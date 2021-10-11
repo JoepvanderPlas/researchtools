a9a9973e72dd08f67cca9c5409c29b479933dc51
Joep van der Plas <J.vdrPlas@tilburguniversity.edu>
2021-10-11 16:54:53 +0200

    save

--- R/estimate_error_correction_SUR.R
+++ R/estimate_error_correction_SUR.R
@@ -36,38 +36,71 @@ function_temp_name <- function(data, y_name, X_name,  time, cross_section) {
   
   ## (5) call itersur
   mod <- itersur(X = X, Y = y, index = index)  #method="FGLS-Praise-Winsten"
-  
+
   
   ## (6) take LT effect 
   # take the coeffs from the object returned by itersur
   DT_coeffs <- setDT(mod@coefficients)
+  # drop the z-scores
+  DT_coeffs[, z := NULL]
   # change the variable name to a character (was a factor)
   DT_coeffs[, variable := as.character(variable)]
-  # create variable_brand combo column. The column variable is actually a variable brand combo name.
-  DT_coeffs[, variable_brand := variable]
+  # create variable_id combo column. The column variable is actually a variable brand combo name.
+  DT_coeffs[, variable_id := variable]
   # extract variable # everything before the last underscore
-  DT_coeffs[, variable := sub("_[^_]+$", "", variable_brand)]
+  DT_coeffs[, variable := sub("_[^_]+$", "", variable_id)]
   # extract the brand # everything after the last underscore
-  DT_coeffs[, (cross_section) := gsub('.*_ ?(\\w+)', '\\1', variable_brand)]
+  DT_coeffs[, (cross_section) := gsub('.*_ ?(\\w+)', '\\1', variable_id)]
+  
+  
+  # take lagged X variables
+  DT_LT_effect <- DT_coeffs[variable %chin% paste0(X_name, "_lag1")]
+  
+  
+  # take speed of adjustment (i) coef & (ii) SE
+  DT_speed_of_adj_temp <- DT_coeffs[variable %like% paste0(y_name, "_lag1")]
+  # remove redudant columns
+  DT_speed_of_adj_temp[, c("variable", "variable_id" ) := NULL]
+  # 
+  setnames(DT_speed_of_adj_temp, c("coef", "se"), c("coef_speed_of_adj", "se_speed_of_adj"))
+  
+  # merge information lagged X & speed of adjustment coefficient
+  DT_LT_effect <- DT_LT_effect[DT_speed_of_adj_temp, on = cross_section]
+  
+  
+  
+  
+  mat_var_covar <- mod@varcovar
+  # 
+  colnames(mat_var_covar) <- DT_coeffs[, variable_id]
+  rownames(mat_var_covar) <- DT_coeffs[, variable_id]
 
   
   
-  #mod@varcovar
+  # loop over cross sections
+  for(id_counter in unique(DT_LT_effect$id)){
+    # dus subset de rows met precies dit paste0(y_name, "_lag1_", id_counter)
+    row_to_select <- paste0(y_name, "_lag1_", id_counter)
+    # subset the columns met (i) variable %like% paste0(y_name, "_lag1") en (ii) de brand name
+    cols_to_select <- DT_LT_effect[id == id_counter, variable_id]
+    vec_var_cov <- mat_var_covar[row_to_select, cols_to_select]
+    DT_var_cov <- data.table(variable_id = names(vec_var_cov), cov_speed_of_adj_and_indep_var = vec_var_cov)
+    # merge 
+    merge(DT_LT_effect, DT_var_cov, all=TRUE)
+  }
   
+  # to do:
+  # (1) check of wat ik hierboven doe juist is
   
-  # DUS PAK DE lag X, voeg daar de lag y aan toe, BELANGRIJK: wat doe je van de var-cov matrix?
+  # (2) compute SE of LT effect
   
-  
-  
-  # create a data table with all statistics required to compute the (i) LT effect and (ii) significance
-  DT_LT_effect <- data.table(variable = DT_coeffs[variable %chin% paste0(X_name, "_lag1"), variable],
-                             coef_indep_var = DT_coeffs[variable %chin% paste0(X_name, "_lag1"), coef],
-                             SE_indep_var = DT_coeffs[variable %chin% paste0(X_name, "_lag1"), se],
-                             coef_speed_of_adj = DT_coeffs[variable %like% paste0(y_name, "_lag1"), coef],
-                             SE_speed_of_adj = DT_coeffs[variable == paste0(y_name, "_lag1"), se],
-                             cov_speed_of_adj_and_indep_var = stats::vcov(mod)[paste0(y_name, "_lag1"), paste0(X_name, "_lag1")])
-  
-  
+                   
+                   
+                   
+                   
+                   
+                   
+                   
   ## estimate with OLS to verify the results
   #data[, id := as.factor(id)]
   #formula <- paste0(paste0(y_name, "_diff1"), " ~ ", " 0 + id + ", paste0(X_name, "_diff1:id", collapse = " + "), " + ", paste0(y_name, "_lag1:id")," + ", paste0(X_name, "_lag1:id", collapse = " + "))
